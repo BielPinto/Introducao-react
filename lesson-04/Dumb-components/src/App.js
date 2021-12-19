df01c2ffa8ff320d900fda4377bd245cea48f4fc
Gabriel Rocha <gabrielp.roch@hotmail.com.br>
2021-12-19 01:20:04 -0300

    playing with Smart components

--- lesson-04/Dumb-components/src/App.js
+++ lesson-04/Dumb-components/src/App.js
@@ -1,42 +1,26 @@
 import React, { useState} from "react";
 import Button from "./components/Button"
 import Calc from "./components/Calc"
+import PhotosGallery from "./components/PhotosGallery";
 
 const App = () => {
-  const [value, setValue] = useState(0)
-  const [min, setMin] = useState(0)
-  const [max, setMax] = useState(30)
+const photos = [
+  "http://placeimg.com/140/60/people",
+  "http://placeimg.com/140/60/animals",
+  "http://placeimg.com/140/60/tech",
+  "http://placeimg.com/140/60/sport",
+  "http://placeimg.com/140/60/ward",
+  "http://placeimg.com/140/60/tecnolog",
+]
 
-  const handleClick = () => {
-    setMin(10)
-  }
-
-  const handleCalc = ({target}) => {
-      const value = target.value
-      setValue(min + max + value)
-  }
-
-  return (
+  return(
     <>
-      <div>
-        <Button
-          onClick={handleClick}
-        >
-            Adicionar button
-        </Button>
-      </div>
-      <br/>
-      <div>
-        <h1>Valor Calculado:{value}</h1>
-        <Calc
-          min={min}
-          max={max}
-          onChange={handleCalc}
-        >
-          
-        </Calc>
-      </div>
+      <h1>Galeria de fotos</h1>
+      <PhotosGallery
+         photos={photos}
+      >
+      </PhotosGallery>
     </>
-  );
+  )
 };
 export default App;
--- lesson-04/Dumb-components/src/components/Calc/index.js
+++ lesson-04/Dumb-components/src/components/Calc/index.js
@@ -12,7 +12,6 @@ function Calc (props) {
            <p>
                 Valor Maximo: {max}
            </p>
-
            <input type="text" onChange={onChange}/>
        </div>
     )
